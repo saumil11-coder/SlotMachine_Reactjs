2baea7ff0abc1ce40e8025d677f556363a6df644
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2022-04-11 18:48:10 +0530

    Add files via upload

---
+++ SlotMach.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+const SlotM=(props)=>
+{
+    // let x='😄';
+    // let y='😄';
+    // let z='🎅';
+    // let x=props.x;
+    // let y=props.y;
+    // let z=props.z;
+    let {x,y,z}=props;
+    if((x===y)&&(y===z))
+    {
+        return(
+            <>
+            <div className="slot_inner">
+            <h1>
+                
+                {x} {y} {z}
+                </h1>
+            <h1 className="Thisis">This is matching</h1>
+            <hr />
+    </div>
+            </>
+);}
+else{
+    return(
+        <>
+        <div className="slot_inner">
+        <h1>
+            
+            {x} {y} {z}
+            </h1>
+        <h1 className="Thisis">This is not  matching</h1>
+        <hr />
+</div>
+        </>
+);
+}
+}
+export default SlotM;
